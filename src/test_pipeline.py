65c4d9ac5ed80c7f74ce932db295edc6cf1de94f
shubhi41 <gargshubhi44@gmail.com>
2026-04-29 12:12:09 +0530

    Update test_pipeline.py
    Just changed Data Path, nothing else

--- src/test_pipeline.py
+++ src/test_pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pipeline import create_pipeline
 
-df=pd.read_csv(r"C:\Users\USER\Documents\AI_Projects\credit-risk-ml-engineer\data\credit_data.csv")
+df=pd.read_csv(r"data\credit_data.csv")
 
 pipeline=create_pipeline(df)
 print("Pipeline created successfully")
